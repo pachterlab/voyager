255ad823628d321c5f1d4d8acb6e225028aba528
Lambda Moses <dlu2@caltech.edu>
2022-04-26 20:18:43 -0700

    Base R (besides ggplot2) reimplementation of plotDimLoadings, to reduce the number of dependencies.

--- R/plot.R
+++ R/plot.R
@@ -524,13 +524,15 @@ moranPlot <- function(sfe, feature, colGraphName, sample_id = NULL,
 #' compared to Seurat. So I'm writing this function to make the elbow plot with
 #' SCE less cumbersome.
 #'
-#' @param sce A \code{SingleCellExperiment} object.
+#' @param sce A \code{SingleCellExperiment} object, or anything that inherits
+#' from \code{SingleCellExperiment}.
 #' @param ndims Number of PCs to plot.
 #' @param reduction Name of the dimension reduction to use. It must have an
 #' attribute called "percentVar". Defaults to "PCA".
 #' @return A ggplot object. The y axis is percentage of variance explained.
 #' @export
 ElbowPlot <- function(sce, ndims = 20, reduction = "PCA") {
+  # For scater::runPCA results
   percent_var <- attr(reducedDim(sce, reduction), "percentVar")
   inds <- seq_len(ndims)
   df <- data.frame(PC = inds,
@@ -538,6 +540,20 @@ ElbowPlot <- function(sce, ndims = 20, reduction = "PCA") {
   ggplot(df, aes(PC, pct_var)) +
     geom_point() +
     labs(x = "PC", y = "Variance explained (%)")
+}
+
+.get_top_loading_genes <- function(df, nfeatures, balanced) {
+  # df has columns gene_show and value
+  if (balanced) {
+    n2 <- floor(nfeatures/2)
+    ord_plus <- order(df$value, decreasing = TRUE)
+    ord_minus <- order(df$value, decreasing = FALSE)
+    out <- c(df[ord_plus[seq_len(n2)],], df[ord_minus[seq_len(n2)],])
+  } else {
+    ord <- order(abs(df$value), decreasing = TRUE)
+    out <- df[ord[seq_len(nfeatures)],]
+  }
+  return(out)
 }
 
 #' Plot top PC loadings of genes
@@ -555,49 +571,47 @@ ElbowPlot <- function(sce, ndims = 20, reduction = "PCA") {
 #' indicate whether to show more human readable gene symbols in the plot instead.
 #' Ignored if the column specified in \code{symbol_col} is absent from rowData.
 #' @param symbol_col If the row names of the gene expression matrix are Ensembl accessions
-#' to avoid ambiguity in analysis, specify
+#' to avoid ambiguity in analysis. If not found in \code{rowData}, then rownames
+#' of the gene count matrix will be used.
 #' @param balanced Return an equal number of genes with + and - scores. If
 #'   FALSE, returns the top genes ranked by the scores absolute values.
 #' @param ncol Number of columns in the facetted plot.
 #' @return A ggplot object. Loadings for different PCs are plotted in different
 #' facets so one ggplot object is returned.
-
+#' @importFrom SingleCellExperiment reducedDim
+#' @importFrom ggplot2 facet_wrap scale_y_discrete
+#' @importFrom stats reorder
 plotDimLoadings <- function(sce, dims = 1:4, nfeatures = 10,
                             show_symbol = TRUE, symbol_col = "symbol",
                             reduction = "PCA",
                             balanced = TRUE, ncol = 2) {
+  # For scater::runPCA results
   loadings <- attr(reducedDim(sce, reduction), "rotation")
-  is_ensembl <- all(str_detect(rownames(sce), "^ENS"))
+  is_ensembl <- all(grepl("^ENS", rownames(sce)))
   if (!is_ensembl) show_symbol <- FALSE # I mean, irrelevant
   loading_cols <- paste0("PC", dims)
   df <- cbind(as.data.frame(rowData(sce)[rownames(loadings),]), loadings[,loading_cols])
-  df <- df %>%
-    pivot_longer(starts_with("PC"), names_to = "PC") %>%
-    group_by(PC)
   if (!symbol_col %in% names(df) || !show_symbol) {
     df$gene_show <- rownames(sce)
   } else {
     df$gene_show <- df[[symbol_col]]
   }
-  if (balanced) {
-    n2 <- floor(nfeatures/2)
-    df_plt_plus <- df %>%
-      slice_max(value, n = n2)
-    df_plt_minus <- df %>%
-      slice_max(-value, n = n2)
-    df_plt <- rbind(df_plt_plus, df_plt_minus)
-  } else {
-    df_plt <- df %>%
-      slice_max(abs(value), n = nfeatures)
-  }
-  df_plt <- df_plt %>%
-    ungroup() %>%
-    mutate(PC = fct_relevel(PC, loading_cols),
-           gene = tidytext::reorder_within(gene_show, value, PC))
+  df_plt <- lapply(loading_cols, function(p) {
+    df_use <- df[,c("gene_show", p)]
+    names(df_use)[2] <- "value"
+    out <- .get_top_loading_genes(df_use, nfeatures, balanced)
+    out$PC <- p
+  })
+  df_plt <- Reduce(rbind, df_plt)
+  df_plt$PC <- factor(df_plt$PC, levels = loading_cols)
+  # Basically reimplementing tidytext::reorder_within and scale_y_reordered
+  df_plt$gene <- paste(df_plt$gene_show, df_plt$PC, sep = "___")
+  df_plt$gene <- reorder(df_plt$gene, df_plt$value)
+  reg <- "___.+$"
   ggplot(df_plt, aes(value, gene)) +
     geom_segment(aes(yend = gene), xend = 0, show.legend = FALSE) +
     geom_point(color = "blue") +
     geom_vline(xintercept = 0, linetype = 2) +
     facet_wrap(~ PC, scales = "free_y", ncol = 2) +
-    tidytext::scale_y_reordered()
+    scale_y_discrete(labels = function(x) gsub(reg, "", x))
 }
