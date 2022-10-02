33c47e523710082f648f609d3c2530eca9d971e3
Lambda Moses <dlu2@caltech.edu>
2022-10-02 16:59:13 -0700

    Updated documentation of spatialReducedDim

--- R/plotSpatialReducedDim.R
+++ R/plotSpatialReducedDim.R
@@ -22,14 +22,21 @@
 #' sfe <- McKellarMuscleData("small")
 #' sfe <- logNormCounts(sfe)
 #' sfe <- runPCA(sfe, ncomponents = 2)
-#' spatialReducedDim(sfe, "PCA", 2, "spotPoly")
+#' spatialReducedDim(sfe, "PCA", 2, "spotPoly",
+#'                   annotGeometryName = "tissueBoundary",
+#'                   divergent = TRUE, diverge_center = 0)
+#' # Basically PC1 separates spots not on tissue from those on tissue.
 spatialReducedDim <- function(sfe, dimred, ncomponents, colGeometryName = 1L,
                               sample_id = NULL, ncol = NULL, ncol_sample = NULL,
+                              annotGeometryName = NULL,
+                              annot_aes = list(), annot_fixed = list(),
+                              exprs_values = "logcounts",
                               aes_use = c("fill", "color", "shape", "linetype"),
                               divergent = FALSE, diverge_center = NULL,
+                              annot_divergent = FALSE,
+                              annot_diverge_center = NULL,
                               size = 0, shape = 16, linetype = 1, alpha = 1,
-                              color = NA, fill = "gray80", show_symbol = TRUE,
-                              ...) {
+                              color = NA, fill = "gray80", ...) {
     aes_use <- match.arg(aes_use)
     sample_id <- .check_sample_id(sfe, sample_id, one = FALSE)
     if (length(ncomponents) == 1L) {
@@ -40,9 +47,9 @@ spatialReducedDim <- function(sfe, dimred, ncomponents, colGeometryName = 1L,
     sample_ind <- colData(sfe)$sample_id %in% sample_id
     values <- as.data.frame(reducedDim(sfe, dimred)[sample_ind,dims_use])
     .plotSpatialFeature(sfe, values, colGeometryName, sample_id, ncol,
-                        ncol_sample, annotGeometryName = NULL,
-                        annot_aes = list(), annot_fixed = list(), aes_use,
-                        divergent, diverge_center, annot_divergent = FALSE,
-                        annot_diverge_center = NULL, size, shape, linetype,
-                        alpha, color, fill, show_symbol,...)
+                        ncol_sample, annotGeometryName,
+                        annot_aes, annot_fixed, aes_use,
+                        divergent, diverge_center, annot_divergent,
+                        annot_diverge_center, size, shape, linetype,
+                        alpha, color, fill, show_symbol = FALSE,...)
 }
