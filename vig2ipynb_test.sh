9cf07ca924437c11b1dd4b75afc9384d832c94b0
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-03-15 18:08:55 -0700

    Update vig2ipynb_test.sh

--- vig2ipynb_test.sh
+++ vig2ipynb_test.sh
@@ -5,6 +5,7 @@ do if [[ "$f" != *"landing"* ]] && [[ "$f" != *"install"* ]]
 then
 arrf=(${f//./ })
 # Try to execute notebook
-jupyter nbconvert --execute .${arrf[0]}.ipynb
+jupyter nbconvert --to html --ExecutePreprocessor.kernel_name=ir --execute .${arrf[0]}.ipynb
+rm .${arrf[0]}.html
 fi
 done
