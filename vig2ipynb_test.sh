98112fdad7bd1fb9517c9d4b1e1aa6f9cd3bef55
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-03-15 17:20:40 -0700

    Create vig2ipynb_test.sh

---
+++ vig2ipynb_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Testrun notebooks
+for f in ./vignettes/*.Rmd
+do if [[ "$f" != *"landing"* ]] && [[ "$f" != *"install"* ]]
+then
+arrf=(${f//./ })
+# Try to execute notebook
+jupyter nbconvert --to 'html' --execute ${arrf[0]}.ipynb
+# Clean up
+rm ${arrf[0]}.html
+fi
+done
