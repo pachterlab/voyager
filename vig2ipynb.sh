840d9ff91f948e9c466a30dba77d93cad5ac9773
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-03-15 17:20:58 -0700

    Move test to separate bash file

--- vig2ipynb.sh
+++ vig2ipynb.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-
 # Generate notebooks
 for f in ./vignettes/*.Rmd
 do if [[ "$f" != *"landing"* ]] && [[ "$f" != *"install"* ]]
 then
 jupytext --to notebook $f
-fi
-done
-
-# Testrun notebooks
-for f in ./vignettes/*.Rmd
-do if [[ "$f" != *"landing"* ]] && [[ "$f" != *"install"* ]]
-then
-arrf=(${f//./ })
-# Try to execute notebook
-jupyter nbconvert --to 'html' --execute ${arrf[0]}.ipynb
-# Clean up
-rm ${arrf[0]}.html
 fi
 done
