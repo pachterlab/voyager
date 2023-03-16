5240f49ef477508fcd76ad27661ad7763613c5b0
Laura Luebbert <56094636+lauraluebbert@users.noreply.github.com>
2023-03-15 17:36:40 -0700

    Update vig2ipynb_test.sh

--- vig2ipynb_test.sh
+++ vig2ipynb_test.sh
@@ -5,7 +5,7 @@ do if [[ "$f" != *"landing"* ]] && [[ "$f" != *"install"* ]]
 then
 arrf=(${f//./ })
 # Try to execute notebook
-jupyter nbconvert --to 'html' --execute ${arrf[0]}.ipynb
+jupyter nbconvert --to 'html' --execute ./${arrf[0]}.ipynb
 # Clean up
 rm ${arrf[0]}.html
 fi
