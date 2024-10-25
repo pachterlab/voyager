59e655f40a925871a0deacaeb6fd68c4aa7618c2
Lambda Moses <dlu2@caltech.edu>
2024-10-24 21:57:46 -0400

    Import S4Vectors functions

--- R/featureData.R
+++ R/featureData.R
@@ -1,3 +1,4 @@
+#' @importFrom S4Vectors mcols metadata metadata<-
 .check_old_params <- function(params, old_params, name, not_check) {
     if (!identical(not_check, NA_character_))
         params <- params[!names(params) %in% not_check]
